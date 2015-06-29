5afc86b7d6a7ea4d81d018f7192c746bed1d1e8b
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-29 15:33:37 +0200

    Init of the project

---
+++ src/GameObject.h
@@ -0,0 +1,15 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#ifndef SFMLTEST_GAMEOBJECT_H
+#define SFMLTEST_GAMEOBJECT_H
+
+
+class GameObject
+{
+
+};
+
+
+#endif //SFMLTEST_GAMEOBJECT_H
