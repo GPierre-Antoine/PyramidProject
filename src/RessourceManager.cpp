525e20409de6e8ca35d974f77a9248fd8c49962e
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-29 16:25:49 +0200

    Ressource Manager

---
+++ src/RessourceManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#include "RessourceManager.h"
---
+++ src/RessourceManager.h
@@ -0,0 +1,19 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#ifndef PYRAMIDPROJECT_RESSOURCEMANAGER_H
+#define PYRAMIDPROJECT_RESSOURCEMANAGER_H
+
+#include <SFML/Graphics/RenderWindow.hpp>
+
+namespace nsRessourceManager
+{
+    class RessourceManager
+    {
+    private:
+        sf::RenderWindow window;
+    };
+
+}
+#endif //PYRAMIDPROJECT_RESSOURCEMANAGER_H
