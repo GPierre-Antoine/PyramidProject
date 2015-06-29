2c67b20d168c72845622e8eac54235614dd86f34
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-29 15:42:26 +0200

    Correction nom projet

--- src/GameObject.h
+++ src/GameObject.h
@@ -2,8 +2,8 @@
 // Created by Pierre-Antoine on 29/06/2015.
 //
 
-#ifndef SFMLTEST_GAMEOBJECT_H
-#define SFMLTEST_GAMEOBJECT_H
+#ifndef PYRAMIDPROJECT_GAMEOBJECT_H
+#define PYRAMIDPROJECT_GAMEOBJECT_H
 namespace nsGameObject
 {
 
--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
 // Created by Pierre-Antoine on 29/06/2015.
 //
 
-#ifndef SFMLTEST_MAIN_H
-#define SFMLTEST_MAIN_H
+#ifndef PYRAMIDPROJECT_MAIN_H
+#define PYRAMIDPROJECT_MAIN_H
 
 #endif //SFMLTEST_MAIN_H
