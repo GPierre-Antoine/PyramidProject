91c4d210d07d84db1ae3f85d6bf799ab9799da3d
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-29 17:21:16 +0200

    Création Animator

---
+++ src/Animator.cpp
@@ -0,0 +1,15 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#include "Animator.h"
+#define RS nsRessourceManager::Animator
+
+RS::Animator ()
+{ }
+
+
+
+
+
+#undef RS
---
+++ src/Animator.h
@@ -0,0 +1,17 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#ifndef PYRAMIDPROJECT_ANIMATOR_H
+#define PYRAMIDPROJECT_ANIMATOR_H
+
+namespace nsRessourceManager
+{
+    class Animator
+    {
+    public:
+        Animator ();
+    };
+
+}
+#endif //PYRAMIDPROJECT_ANIMATOR_H
