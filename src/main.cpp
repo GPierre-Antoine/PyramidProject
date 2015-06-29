8e39037027e43baa565c02dc5633933630e3abdf
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-29 13:46:45 +0200

    Init of the project

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+#include <SFML/Graphics.hpp>
+
+
+//#include "main.h"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Pierre-Antoine on 29/06/2015.
+//
+
+#ifndef SFMLTEST_MAIN_H
+#define SFMLTEST_MAIN_H
+
+#endif //SFMLTEST_MAIN_H
