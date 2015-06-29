084c5ddf5100ce70703eedd7c80e755b24caeda6
Jeanjean Theo <theo.simba@live.fr>
2015-06-30 01:58:38 +0200

    Update RessourceManager.cpp

--- src/RessourceManager.cpp
+++ src/RessourceManager.cpp
@@ -3,13 +3,26 @@
 //
 
 #include "RessourceManager.h"
+
 #define RS nsRessourceManager::RessourceManager
 //using namespace sf;
 
 
-RS::RessourceManager ()
+RS::RessourceManager()
 {
 
+}
+
+void RS::loadCharacterTextures(std::string imageFileName, const sf::Vector2i & tailleDecoupe)
+{
+}
+
+std::vector<const sf::Texture>* RS::getTabPointer(int line)
+{
+    std::vector<const sf::Texture> tab;
+    tab = textures;
+    //return &textures[line];
+    return nullptr;
 }
 
 #undef  RS
