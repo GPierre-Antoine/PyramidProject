4a9b343e9989b8bfd37569c403861c1056f32d9f
Theo <theo.jeanjean42@gmail.com>
2015-06-30 01:50:04 +0200

    Interface Character

--- src/RessourceManager.h
+++ src/RessourceManager.h
@@ -6,18 +6,38 @@
 #define PYRAMIDPROJECT_RESSOURCEMANAGER_H
 
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <c++/bits/stl_map.h>
 
 namespace nsRessourceManager
 {
+    //Pour la taille du tableau de textures, actuellement on a Martine, Momie donc 2
+    const int CHARACTER_NUMBER = 2;
+
     class RessourceManager
     {
     private:
-        static sf::RenderWindow window;
-        static std::vector<sf::Texture> textures;
+        //Un pointeur vers la fenetre de jeu pour dessiner
+        static sf::RenderWindow* window;
+
+        //Old one
+        //static std::vector<const sf::Texture> textures;
+
+        //contient des tableaux de textures de personnages, 0 car tableau vide au debut,
+        //on ajoutera les 12 textures par l'utilisation d'un push_back
+        static std::vector<std::vector<const sf::Texture>> textures[CHARACTER_NUMBER][12];
 
     public:
-        RessourceManager ();
+        void loadCharacterTextures(std::string imageFileName, const sf::Vector2i & tailleDecoupe);
+        std::vector<const sf::Texture>* getTabPointer (int line);
+
+        RessourceManager();
+    };
+
+    enum CharacterTextures
+    {
+        MARTINE =2
     };
 
 }
+
 #endif //PYRAMIDPROJECT_RESSOURCEMANAGER_H
--- src/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Pierre-Antoine on 29/06/2015.
-//
-
-#ifndef PYRAMIDPROJECT_MAIN_H
-#define PYRAMIDPROJECT_MAIN_H
-
-#endif //SFMLTEST_MAIN_H
