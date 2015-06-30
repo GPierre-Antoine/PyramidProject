5dd7ad8098935c4332638b1bcdeb6f0003d86404
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-30 16:38:54 +0200

    QuadTree Files created

---
+++ src/nsGameEngine/QuadTree.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Pierre-Antoine on 30/06/2015.
+//
+
+#include "QuadTree.h"
+#define GE nsGameEngine::Quadtree
+
+#undef  GE
---
+++ src/nsGameEngine/QuadTree.h
@@ -0,0 +1,16 @@
+//
+// Created by Pierre-Antoine on 30/06/2015.
+//
+
+#ifndef PYRAMIDPROJECT_QUADTREE_H
+#define PYRAMIDPROJECT_QUADTREE_H
+namespace nsGameEngine
+{
+
+    class QuadTree
+    {
+
+    };
+}
+
+#endif //PYRAMIDPROJECT_QUADTREE_H
