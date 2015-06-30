404cb58861c179b3f600efe463af72c44f6250e7
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-06-30 16:12:04 +0200

    Added Game Class

---
+++ src/nsGameEngine/Game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pierre-Antoine on 30/06/2015.
+//
+
+#include "Game.h"
---
+++ src/nsGameEngine/Game.h
@@ -0,0 +1,15 @@
+//
+// Created by Pierre-Antoine on 30/06/2015.
+//
+
+#ifndef PYRAMIDPROJECT_GAME_H
+#define PYRAMIDPROJECT_GAME_H
+
+
+class Game
+{
+
+};
+
+
+#endif //PYRAMIDPROJECT_GAME_H
