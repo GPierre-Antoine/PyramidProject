22b4c9c0b2f1879d8c62929610a826ff236ff79c
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-07-01 01:02:59 +0200

    Hierarchy GO >> Entity >> Character >>Player

---
+++ src/nsGameEngine/Fabric.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Pierre-Antoine on 01/07/2015.
+//
+
+#include "Fabric.h"
+#define GE nsGameEngine::Fabric
+#undef GE
---
+++ src/nsGameEngine/Fabric.h
@@ -0,0 +1,24 @@
+//
+// Created by Pierre-Antoine on 01/07/2015.
+//
+
+#ifndef PYRAMIDPROJECT_FABRIC_H
+#define PYRAMIDPROJECT_FABRIC_H
+
+namespace nsGameEngine
+{
+
+    class Fabric
+    {
+    //La durée de vie de la fabrique est celle du programme.
+        //singleton
+
+        //fabrique
+
+        //Utilisé avec Prototype (implémenté dans GameObject
+
+    };
+}
+
+
+#endif //PYRAMIDPROJECT_FABRIC_H
