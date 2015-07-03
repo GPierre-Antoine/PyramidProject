03672b546385ee34451298447b749fe44bbf3db5
Pierre-Antoine Guillaume <pierreantoine.guillaume+github@gmail.com>
2015-07-03 16:23:59 +0200

    Collider Compillable, Interface.h modifié : Collidable est maintenant définie en UInt16

--- src/nsGameEngine/Collision/Collider.h
+++ src/nsGameEngine/Collision/Collider.h
@@ -12,22 +12,25 @@
     {
         namespace nsCollider
         {
+            template <typename T>
             class Collider;
+            template <typename T>
             class Rectangle;
+            template <typename T>
             class Circle;
 
             template <typename T>
             class Collider
             {
             private:
-                virtual bool                collidesV     (const Rectangle & other) const noexcept = 0;
-                virtual bool                OtherfitsInto (const Rectangle & other) const noexcept = 0;
+                virtual bool                collidesV       (const Rectangle<T> & other) const noexcept = 0;
+                virtual bool                OtherfitsInto   (const Rectangle<T> & other) const noexcept = 0;
 
-                virtual bool                collidesV     (const Circle    & other) const noexcept = 0;
-                virtual bool                otherFitsInto (const Circle    & other) const noexcept = 0;
+                virtual bool                collidesV       (const Circle<T>    & other) const noexcept = 0;
+                virtual bool                otherFitsInto   (const Circle<T>    & other) const noexcept = 0;
             public:
-                bool                        collides (const Collider & other) const noexcept;
-                bool                        fitsInto (const Collider & other) const noexcept;
+                bool                        collides        (const Collider<T> & other) const noexcept;
+                bool                        fitsInto        (const Collider<T> & other) const noexcept;
                 virtual void move (const sf::Vector2<T> & mvt) noexcept = 0;
                 virtual void move (const T & pX, const T & pY) noexcept = 0;
 
@@ -36,24 +39,24 @@
             };
 
             template <typename T>
-            class Rectangle : public Collider
+            class Rectangle : public Collider<T>
             {
             private:
-                friend class Circle;
+                friend class Circle<T>;
                 T                           width;
                 T                           height;
                 sf::Vector2<T>              origin;
 
-                virtual bool                otherFitsInto (const Rectangle & other) const noexcept;
-                virtual bool                collidesV     (const Rectangle & other) const noexcept;
+                virtual bool                otherFitsInto   (const Rectangle<T> & other) const noexcept;
+                virtual bool                collidesV       (const Rectangle<T> & other) const noexcept;
 
-                virtual bool                otherFitsInto (const Circle    & other) const noexcept;
-                virtual bool                collidesV     (const Circle    & other) const noexcept;
+                virtual bool                otherFitsInto   (const Circle<T>    & other) const noexcept;
+                virtual bool                collidesV       (const Circle<T>    & other) const noexcept;
 
             public:
                 //Constructor
-                Rectangle (const T & x, const T & y,     const T & width, const T & height) noexcept;
-                Rectangle (const sf::Vector2<T>& origin, const T & width, const T & height) noexcept;
+                Rectangle (const T & x, const T & y,        const T & width, const T & height) noexcept;
+                Rectangle (const sf::Vector2<T>& origin,    const T & width, const T & height) noexcept;
 
                 virtual ~Rectangle () {}
             };
@@ -62,18 +65,18 @@
 
 
             template <typename T>
-            class Circle : public Collider
+            class Circle : public Collider<T>
             {
             private:
-                friend class Rectangle;
-                T                           radius;
-                sf::Vector2<T>              origin;
+                friend class Rectangle<T>;
+                T                                           radius;
+                sf::Vector2<T>                              origin;
 
-                virtual bool otherFitsInto (const Rectangle & other) const noexcept;
-                virtual bool collidesV (const Rectangle & other) const noexcept;
+                virtual bool otherFitsInto                  (const Rectangle<T> & other) const noexcept;
+                virtual bool collidesV                      (const Rectangle<T> & other) const noexcept;
 
-                virtual bool otherFitsInto (const Circle    & other) const noexcept;
-                virtual bool collidesV (const Circle    & other) const noexcept;
+                virtual bool otherFitsInto                  (const Circle<T>    & other) const noexcept;
+                virtual bool collidesV                      (const Circle<T>    & other) const noexcept;
 
             public:
                 //Constructor
