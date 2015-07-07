7ca0a63872b40c34282babe9cb59fce12d79ca6c
Jeanjean Theo <theo.simba@live.fr>
2015-07-07 20:19:03 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,18 @@
 // Created by Pierre-Antoine on 29/06/2015.
 //
 
+#include "VisualTest.h"
 #include "UnitTest.h"
 
-#define TEST nsTest::UnitTest
+#define UNIT nsTest::UnitTest
+#define VISUAL nsTest::VisualTest
 
 int main()
 {
-    //TEST::PlayerAnimation1();
-    TEST::ColliderTest();
+    UNIT::ColliderTest();
+
+    //Un seul visual a la fois a choisir
+    VISUAL::FluiditeMovement();
 
     return 0;
 }
