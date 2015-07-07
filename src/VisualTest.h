ee7c9f6aa79726f468dd40095ab08e21bf95e8bf
Jeanjean Theo <theo.simba@live.fr>
2015-07-07 20:15:41 +0200

    Update VisualTest.h

--- src/VisualTest.h
+++ src/VisualTest.h
@@ -1,1 +1,18 @@
+//
+// Created by Theo on 07/07/2015.
+//
 
+#ifndef PYRAMIDPROJECT_VISUALTESTS_H
+#define PYRAMIDPROJECT_VISUALTESTS_H
+
+namespace nsTest
+{
+    class VisualTest
+    {
+    public:
+        static void PlayerAnimation1();
+        static void FluiditeMovement();
+    };
+}
+
+#endif //PYRAMIDPROJECT_VISUALTESTS_H
