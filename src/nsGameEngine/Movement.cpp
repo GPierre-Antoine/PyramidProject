3188812953e96b1ca42c0e77c9b272d4c1e2a747
Theo <theo.jeanjean42@gmail.com>
2015-07-09 22:17:49 +0200

    creation classe Movement

---
+++ src/nsGameEngine/Movement.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Theo on 09/07/2015.
+//
+
+#include "Movement.h"
---
+++ src/nsGameEngine/Movement.h
@@ -0,0 +1,15 @@
+//
+// Created by Theo on 09/07/2015.
+//
+
+#ifndef PYRAMIDPROJECT_MOVEMENT_H
+#define PYRAMIDPROJECT_MOVEMENT_H
+
+
+class Movement
+{
+
+};
+
+
+#endif //PYRAMIDPROJECT_MOVEMENT_H
